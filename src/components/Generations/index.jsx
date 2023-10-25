782c482ecfccf204c67fd692cb02de14d015a557
Slyxes5 <avantakun@gmail.com>
2023-10-25 11:47:10 +0800

    update

--- src/components/Generations/index.jsx
+++ src/components/Generations/index.jsx
@@ -1,5 +1,20 @@
+import GenSatu from "./Gen1";
+import GenDua from "./Gen2";
+import GenTiga from "./Gen3";
+import GenEmpat from "./Gen4";
+
 const Generations = () => {
-    return <h2>Generations</h2>;
+    return(
+        <>
+        <h2>Generations</h2>
+        <GenSatu/>
+        <GenDua/>
+        <GenTiga/>
+        <GenEmpat/>
+        
+        
+        </>
+    )
 };
 
 export default Generations
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,16 @@
+import Logo from "../Logo";
+import Generations from "../Generations";
+
 const Header = () => {
-    return <h1>Header</h1>;
+    return(
+        <>
+        <h1>Header</h1>
+        <Logo/>
+        <Generations/>
+
+        </>
+    )
 };
+
 
 export default Header
